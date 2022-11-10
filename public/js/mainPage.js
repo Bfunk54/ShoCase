ea0693f40b5d171a1ac22975783a3cb683981318
Bfunk54 <ben_fein@icloud.com>
2022-11-10 12:55:40 -0500

    added new js file and added some new html on main

---
+++ public/js/mainPage.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+
+
+instance.open();
+
+instance.close();
