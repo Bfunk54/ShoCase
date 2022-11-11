8cb010a1bf398f7b0ff0f715f3bf851fd50d28bd
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-11 10:20:02 -0500

    user seeds setup

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,16 +1,16 @@
 const sequelize = require('../config/config');
 const seedUsers = require('./userData.js');
-const seedPosts = require('./postData.js');
-const seedComments = require('./commentData.js');
+// const seedPosts = require('./postData.js');
+// const seedComments = require('./commentData.js');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedUsers();
 
-  await seedPosts();
+  // await seedPosts();
 
-  await seedComments();
+  // await seedComments();
 
   process.exit(0);
 };
