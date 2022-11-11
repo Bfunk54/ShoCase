92370bbdb2608d0fc50d6502000dfc7b9d48fb0f
Bfunk54 <ben_fein@icloud.com>
2022-11-10 20:54:31 -0500

    added a lot more css and front-end fixes

--- models/index.js
+++ models/index.js
@@ -1,32 +1,32 @@
-const User = require('./User');
-const Playlist = require('./Playlist');
-const Comment = require('./Comment')
+// const User = require('./User');
+// const Playlist = require('./Playlist');
+// const Comment = require('./Comment')
 
-// User.hasMany(Post, {
-//     foreignKey: 'user_id',
-//     onDelete: 'CASCADE'
-// });
+// // User.hasMany(Post, {
+// //     foreignKey: 'user_id',
+// //     onDelete: 'CASCADE'
+// // });
 
-// User.hasMany(Comment, {
-//     foreignKey: 'user_id',
-//     onDelete: 'CASCADE'
-// });
+// // User.hasMany(Comment, {
+// //     foreignKey: 'user_id',
+// //     onDelete: 'CASCADE'
+// // });
 
-// Playlist.belongsTo(User, {
-//     foreignKey: 'user_id'
-// });
+// // Playlist.belongsTo(User, {
+// //     foreignKey: 'user_id'
+// // });
 
-// Comment.belongsTo(User, {
-//     foreignKey: 'user_id'
-// });
+// // Comment.belongsTo(User, {
+// //     foreignKey: 'user_id'
+// // });
 
-// Post.hasMany(Comment, {
-//     foreignKey: 'post_id',
-//     onDelete: 'CASCADE'
-// });
+// // Post.hasMany(Comment, {
+// //     foreignKey: 'post_id',
+// //     onDelete: 'CASCADE'
+// // });
 
-// Comment.belongsTo(Post, {
-//     foreignKey: 'post_id'
-// });
+// // Comment.belongsTo(Post, {
+// //     foreignKey: 'post_id'
+// // });
 
-module.exports = { User, Playlist, Comment };
+// module.exports = { User, Playlist, Comment };
