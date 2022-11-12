491d88e0eec46a0200e99192584256f213fc80cf
Bfunk54 <ben_fein@icloud.com>
2022-11-12 11:14:55 -0500

    added event listeners for all add buttons

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,5 +1,10 @@
 const jikanUrl = 'https://api.jikan.moe/v4/anime?q=';
 const jikanOptions = {limit: '&limit_10', min_score: '&min_score'};
+const anime1Btn = document.querySelector('#anime1');
+const anime2Btn = document.querySelector('#anime2');
+const anime3Btn = document.querySelector('#anime3');
+const anime4Btn = document.querySelector('#anime4');
+
 
 // Access the sidenav and add the javascript for it
 document.addEventListener('DOMContentLoaded', function() {
@@ -16,3 +21,47 @@ document.addEventListener('DOMContentLoaded', function() {
     const firstAnimeName  = data;
 
   }
+
+anime1Btn.addEventListener("submit", e => {
+    e.preventDefault();
+    animeSearch = search.value.trim();
+
+    if (!animeSearch) {
+        alert("Please enter an anime name");
+    } else {
+        startSearch(animeSearch);
+    }
+})
+
+anime2Btn.addEventListener("submit", e => {
+  e.preventDefault();
+  animeSearch = search.value.trim();
+
+  if (!animeSearch) {
+      alert("Please enter an anime name");
+  } else {
+      startSearch(animeSearch);
+  }
+})
+
+anime2Btn.addEventListener("submit", e => {
+  e.preventDefault();
+  animeSearch = search.value.trim();
+
+  if (!animeSearch) {
+      alert("Please enter an anime name");
+  } else {
+      startSearch(animeSearch);
+  }
+})
+
+anime2Btn.addEventListener("submit", e => {
+  e.preventDefault();
+  animeSearch = search.value.trim();
+
+  if (!animeSearch) {
+      alert("Please enter an anime name");
+  } else {
+      startSearch(animeSearch);
+  }
+})
