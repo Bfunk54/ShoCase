211ecf4e305e48c7b8a648250bfbde873a722798
Bfunk54 <ben_fein@icloud.com>
2022-11-15 21:27:43 -0500

    a whole bunch of css and js troubleshooting to get likes to work

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,112 +1,123 @@
-const router = require('express').Router();
-const { Playlist, Comment, User, Anime, Favorites } = require('../models');
-const withAuth = require('../utils/auth');
-const Sequelize = require('sequelize');
+const router = require("express").Router();
+const { Playlist, Comment, User, Anime, Favorites } = require("../models");
+const withAuth = require("../utils/auth");
+const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
-
 // get all playlists for homepage
-router.get('/', async (req, res) => {
-    try {
-        // Get all posts and JOIN with user data
-        if (req.session.user_id) {
-            const playlistData = await Playlist.findAll({
-                include: [{ model: User }, { model: Anime }, { model: Favorites }],
-                attributes: {
-                    include: [
-                        [
-                            Sequelize.literal(`(
+router.get("/", async (req, res) => {
+  try {
+    // Get all posts and JOIN with user data
+    if (req.session.user_id) {
+      const playlistData = await Playlist.findAll({
+        include: [{ model: User }, { model: Anime }, { model: Favorites }],
+        attributes: {
+          include: [
+            [
+              Sequelize.literal(`(
                                 SELECT COUNT(*)
                                 FROM Favorites AS favorites
                                 WHERE
                                     playlist.id = playlist_id
                             )`),
-                            'favoritesCount'
-                        ],
-                        [
-                            Sequelize.literal(`(
+              "favoritesCount",
+            ],
+            [
+              Sequelize.literal(`(
                                 SELECT COUNT(*) FROM Favorites AS checks WHERE playlist.id = playlist_id AND ${req.session.user_id} = user_id
                             )`),
-                            'hasFavorited'
-                        ]
-                    ]
-                }
-            });
-            const playlists = playlistData.map((playlist) => playlist.get({ plain: true }));
-            const favoritesData = await Favorites.findAll({
-                where: {
-                    user_id: req.session.user_id
-                }
-            });
+              "hasFavorited",
+            ],
+          ],
+        },
+      });
+      const playlists = playlistData.map((playlist) =>
+        playlist.get({ plain: true })
+      );
+      const favoritesData = await Favorites.findAll({
+        where: {
+          user_id: req.session.user_id,
+        },
+      });
 
+      const favorites = favoritesData.map((favorite) =>
+        favorite.get({ plain: true })
+      );
 
-            const favorites = favoritesData.map((favorite) => favorite.get({ plain: true }));
-
-
-            res.render('all-playlists', {
-                favorites,
-                playlists,
-                loggedIn: req.session.loggedIn
-            });
-        } else {
-            const playlistData = await Playlist.findAll({
-                include: [{ model: User }, { model: Anime }, { model: Favorites }],
-                attributes: {
-                    include: [
-                        [
-                            Sequelize.literal(`(
+      res.render("all-playlists", {
+        favorites,
+        playlists,
+        loggedIn: req.session.loggedIn,
+      });
+    } else {
+      const playlistData = await Playlist.findAll({
+        include: [{ model: User }, { model: Anime }, { model: Favorites }],
+        attributes: {
+          include: [
+            [
+              Sequelize.literal(`(
                             SELECT COUNT(*)
                             FROM Favorites AS favorites
                             WHERE
                                 playlist.id = playlist_id
                         )`),
-                            'favoritesCount'
-                        ],
-                    ]
-                }
-            })
-            const playlists = playlistData.map((playlist) => playlist.get({ plain: true }));
-            console.log(playlists)
+              "favoritesCount",
+            ],
+          ],
+        },
+      });
+      const playlists = playlistData.map((playlist) =>
+        playlist.get({ plain: true })
+      );
 
-
-            res.render('all-playlists', {
-                playlists,
-                loggedIn: req.session.loggedIn
-            });
-        };
-
-    } catch (err) {
-        res.status(500).json(err);
+      res.render("all-playlists", {
+        playlists,
+        loggedIn: req.session.loggedIn,
+      });
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get playlists by search
-router.get('/playlists/search/:search', async (req, res) => {
-    try {
-        const playlistData = await Playlist.findAll({ where: { title: { [Op.like]: '%' + req.params.search + '%' } } })
-        const playlists = playlistData.map((playlist) => playlist.get({ plain: true }))
-        res.render('playlist-search', { playlists, loggedIn: req.session.loggedIn })
-    } catch (err) {
-        res.status(500).json(err);
-    }
+router.get("/playlists/search/:search", async (req, res) => {
+  try {
+    const playlistData = await Playlist.findAll({
+      where: { title: { [Op.like]: "%" + req.params.search + "%" } },
+    });
+    const playlists = playlistData.map((playlist) =>
+      playlist.get({ plain: true })
+    );
+    res.render("playlist-search", {
+      playlists,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get single playlist by id
-router.get('/playlist/:id', withAuth, async (req, res) => {
-    try {
-        const playlistData = await Playlist.findByPk(req.params.id, {
-            include: [{ model: User }, { model: Anime }, { model: Comment, include: [{ model: User }] }]
-        });
+router.get("/playlist/:id", withAuth, async (req, res) => {
+  try {
+    const playlistData = await Playlist.findByPk(req.params.id, {
+      include: [
+        { model: User },
+        { model: Anime },
+        { model: Comment, include: [{ model: User }] },
+      ],
+    });
 
-        const playlist = playlistData.get({ plain: true });
+    const playlist = playlistData.get({ plain: true });
 
-        res.render('single-playlist', {
-            ...playlist,
-            loggedIn: req.session.loggedIn
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    res.render("single-playlist", {
+      ...playlist,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //get all favorites
@@ -117,10 +128,8 @@ router.get('/playlist/:id', withAuth, async (req, res) => {
 //             include: [{ model: User }, { model: Anime }, { model: Favorites }],
 //         });
 
-
 //         // Serialize data so the template can read it
 //         const favorites = favoritesData.map((playlist) => playlist.get({ plain: true }));
-
 
 //         // Pass serialized data and session flag into template
 //         res.render('all-playlists', {
@@ -132,30 +141,29 @@ router.get('/playlist/:id', withAuth, async (req, res) => {
 //     }
 // });
 
-
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('login');
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
 });
 
-router.get('/create-playlists', withAuth, (req, res) => {
-    res.render('create-playlists');
+router.get("/create-playlists", withAuth, (req, res) => {
+  res.render("create-playlists");
 });
 
-router.get('/signup', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('signup');
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
 });
 
-router.get('/about-us', (req, res) => {
-    res.render('about-us', {
-        loggedIn: req.session.loggedIn
-    });
+router.get("/about-us", (req, res) => {
+  res.render("about-us", {
+    loggedIn: req.session.loggedIn,
+  });
 });
 module.exports = router;
--- public/js/favorites.js
+++ public/js/favorites.js
@@ -1,60 +1,62 @@
 const favoritePlaylist = async (id) => {
-    playlist_id = id;
-    await fetch(`/api/favorites`, {
-        method: 'POST',
-        body: JSON.stringify({
-            playlist_id
-        }),
+  playlist_id = id;
+  await fetch(`/api/favorites`, {
+    method: "POST",
+    body: JSON.stringify({
+      playlist_id,
+    }),
     headers: {
-            'Content-Type': 'application/json'
-        }
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.user_id) {
+        response.json();
+      } else {
+        document.location.replace("/login");
+      }
     })
-    .then(response => response.json())
     .then(document.location.reload())
-    .catch(err => console.log(err));
-}
-
-const removeFavorite = async (id) => {
-    await fetch(`/api/favorites/${id}`, {
-        method: `DELETE`
-    }).then(response => response.json())
-    .then(document.location.reload())
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
-
-
-
-
+const removeFavorite = async (id) => {
+  await fetch(`/api/favorites/${id}`, {
+    method: `DELETE`,
+  })
+    .then((response) => response.json())
+    .then(document.location.reload())
+    .catch((err) => console.log(err));
+};
 
 const handlePlaylistFavorite = async (e) => {
-    // Prevents the click listener for the list from being called when the button inside of it is clicked
-    e.stopPropagation();
-    
-    const playlist = e.target;
-    const playlistId = playlist.parentElement.getAttribute('data-id');
+  // Prevents the click listener for the list from being called when the button inside of it is clicked
+  e.stopPropagation();
 
+  const playlist = e.currentTarget;
+  const playlistId = playlist.parentElement.getAttribute("data-id");
 
-    const response = await favoritePlaylist(playlistId);
+  const response = await favoritePlaylist(playlistId);
 
-    return response;
-
+  return response;
 };
 
 const handleRemoveFavorite = async (e) => {
-    e.stopPropagation();
+  e.stopPropagation();
 
-    const playlist = e.target;
-    const playlistId = playlist.parentElement.getAttribute('data-id');
+  const playlist = e.currentTarget;
+  const playlistId = playlist.parentElement.getAttribute("data-id");
 
-    const response = await removeFavorite(playlistId);
+  const response = await removeFavorite(playlistId);
 
-    return response;
-}
+  return response;
+};
 
-
-
-const favoriteBtn = document.querySelectorAll('.favorite-btn')
-favoriteBtn.forEach((btn) => btn.addEventListener('click', handlePlaylistFavorite));
-const alreadyFavoritedBtn = document.querySelectorAll('.already-favorited-btn')
-alreadyFavoritedBtn.forEach((btn) => btn.addEventListener('click', handleRemoveFavorite));
+const favoriteBtn = document.querySelectorAll(".favorite-btn");
+favoriteBtn.forEach((btn) =>
+  btn.addEventListener("click", handlePlaylistFavorite)
+);
+const alreadyFavoritedBtn = document.querySelectorAll(".already-favorited-btn");
+alreadyFavoritedBtn.forEach((btn) =>
+  btn.addEventListener("click", handleRemoveFavorite)
+);
